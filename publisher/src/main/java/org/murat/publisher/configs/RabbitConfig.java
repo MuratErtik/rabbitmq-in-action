f3246a42c6b96a959bd9420cdcc24fb7c4e5f01e
Murat Ertik <dmuratertik1@gmail.com>
2026-06-29 21:01:19 +0300

    DLQ added.

--- publisher/src/main/java/org/murat/publisher/configs/RabbitConfig.java
+++ publisher/src/main/java/org/murat/publisher/configs/RabbitConfig.java
@@ -1,15 +1,42 @@
 package org.murat.publisher.configs;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
 
+//    @Bean
+//    public Queue exampleQueue(){
+//        // durable: false, exclusive: false, autoDelete: false
+//        return new Queue("example-queue", false, false, false);
+//    }
+
     @Bean
-    public Queue exampleQueue(){
-        // durable: false, exclusive: false, autoDelete: false
-        return new Queue("example-queue", false, false, false);
+    public Queue exampleQueue() {
+        return QueueBuilder.durable("example-queue") // durable means if rabbitmq restart it is still there
+                .withArgument("x-dead-letter-exchange", "")            // default exchange
+                .withArgument("x-dead-letter-routing-key", "example-queue.dlq")
+                .build();
+    }
+
+    //with this bean able to sending DTO to consumer
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    //using for sending a message.
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter converter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(converter);
+        return template;
     }
 }
